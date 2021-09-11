6cff9e643fc9dd8f3aa55f02fa48012cf61941eb
Shaun Teo <42372370+shaun97@users.noreply.github.com>
2021-09-11 22:40:55 +0800

    split contract into 2

--- tictactoeV2.sol
+++ tictactoeV2.sol
@@ -13,6 +13,20 @@ contract TicTacToe {
     enum Status { WAITING_FOR_PLAYER, IN_PROGRESS, HOST_PLAYER_WON, OTHER_PLAYER_WON, DRAW }
     enum GameType { BOT, PLAYER }
     
+    struct Game {
+        address playerOne; // Player 1 X
+        address playerTwo; // Player 2 other_player
+        
+        Symbol playerOneSymbol;
+        Symbol playerTwoSymbol;
+        
+        Status status;
+        GameType gameType;
+        
+        Symbol[9] board;
+        
+    }
+    
     struct PlayerBoard {
         address host_player; // Player 1 X
         address other_player; // Player 2 O
@@ -29,10 +43,41 @@ contract TicTacToe {
         Symbol[9] gameboard;
     }
     
+    struct Player { //keep it to top 10?
+        address player;
+        uint256 score;
+    }
+    
     // mapping to store the player's board
     mapping(address => PlayerBoard) public gameboards;
+    mapping(address => uint256) public scoreboard;
+    mapping (uint256 => Player) public leaderboard;
+    mapping(uint256 => gameID) public scoreboard;
+    
     address[] public players;
-
+    
+    //Function to check if the wager sum is evenly matched
+    constructor() public payable {
+        require(msg.value > 0, "Must bet a positive amount");
+    }
+    
+    function viewBet() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function matchBet() external payable {
+        require(msg.value == address(this).balance, "Must match original bet");
+    }
+    
+    //Adding a storage value for the wager
+    mapping(address => uint256) public wagers_;
+    
+    //Function to set the wager 
+    function placeWager() external payable {
+    require(msg.sender == host_player || msg.sender == other_player);
+    wagers_[msg.sender] = msg.value;
+    }
+    
     function start_game(bool isBot) public returns (bool) {
         
         gameboards[msg.sender] = PlayerBoard({
@@ -71,6 +116,40 @@ contract TicTacToe {
     function get_board() public view returns (Symbol[9] memory) {
         return gameboards[msg.sender].gameboard;
     }
+    
+    function newGame() public returns (uint256 gameId) {
+        Game memory game;
+        game.playerTurn = players.host_player;
+        numberOfGames++;
+        games[numberOfGames] = game;
+        emit GameCreated(nrOfGames, msg.sender);
+        return numberOfGames;
+
+    }
+
+    function joinGame(uint256 _gameId) public returns (bool success, string reason) {
+
+        if (_gameId > numberOfGames) {
+            return (false, "No such game exists.");
+        }
+        address player = msg.sender;
+        Game storage game = games[_gameId];
+        // Assign the new player to slot 1 if it is still available.
+        if (game.host_player == address(0)) {
+            game.host_player = player;
+            emit PlayerJoinedGame(_gameId, player, uint8(players.host_player));
+            return (true, "Joined as host player");
+        }
+        // If slot 1 is taken, assign the new player to slot 2 if it is still available.
+        if (game.other_player == address(0)) {
+            game.other_player = player;
+            emit PlayerJoinedGame(_gameId, player, uint8(players.other_player));
+            return (true, "Joined as other player. Player one can make the first move.");
+
+        }
+        return (false, "All seats taken.");
+    }
+
 
     function bot_move(Symbol[9] memory gameboard, Symbol bot_symbol, Symbol player_symbol) pure internal returns(int) {
         
@@ -144,6 +223,9 @@ contract TicTacToe {
         //check win
         if (evaluate(board.gameboard, board.host_player_symbol, board.other_player_symbol) == 1) {
             board.status = Status.HOST_PLAYER_WON;
+            update_scoreboard();
+            update_leaderboard();
+            host_player.transfer(wagers_[host_player]); //Host player wins - Wager goes to host player
             return "win";
         } else if (is_moves_left(board.gameboard) == false) {
             return "draw";
@@ -155,6 +237,7 @@ contract TicTacToe {
             board.gameboard[uint256(move)] = board.other_player_symbol;
             if (evaluate(gameboards[msg.sender].gameboard, board.other_player_symbol, board.host_player_symbol) == 1) {
                 board.status = Status.OTHER_PLAYER_WON;
+                other_player.transfer(wagers_[other_player]); //Other player wins - Wager goes to other player
                 return "lost";
             }
             
@@ -169,7 +252,7 @@ contract TicTacToe {
     
     
     function evaluate(Symbol[9] memory gameboard, Symbol player, Symbol opponent) internal pure returns(int) {
-                uint8[3][8] memory winning_states = [
+        uint8[3][8] memory winning_states = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [6, 4, 2]
@@ -203,5 +286,43 @@ contract TicTacToe {
     
     function generate_random_start() internal view returns(uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players))) % 2;
+    }
+    
+    
+    //=============leaderboard=============
+    function update_scoreboard() public {
+        scoreboard[msg.sender] += 1;
+    }
+    
+    function get_score(address player) public view returns(uint256) {
+        return scoreboard[player];
+    }
+    
+    function update_leaderboard() public {
+        uint maxLen = 10;
+        uint256 player_score = get_score(msg.sender);
+
+        if (player_score > leaderboard[maxLen-1].score) {
+            for (uint i = 0; i < maxLen; i ++) {
+                if (player_score > leaderboard[i].score) {
+                    if (msg.sender == leaderboard[i].player) {
+                        leaderboard[i].score = player_score;
+                    } else { //shift down
+                        Player memory curr_player = leaderboard[i];
+                        for (uint j = i + 1; j < maxLen + 1; j ++) {
+                            Player memory next_player = leaderboard[j];
+                            leaderboard[j] = curr_player;
+                            curr_player = next_player;
+                        }
+                        
+                        leaderboard[i] = Player({
+                            player: msg.sender,
+                            score: player_score
+                        });
+                    }
+                }
+            }
+            delete leaderboard[maxLen];
+        }
     }
 } 
